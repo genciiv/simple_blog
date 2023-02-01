e526760cc8aa9764c85de7e51b7b0a01197e1733
G-code <vaqogenci@gmail.com>
2023-02-01 12:55:59 +0100

    simple blog

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmyYR5Mv4MOg6WaeCcvSCbOXjCOo7dyrA",
+  authDomain: "blogproject-93123.firebaseapp.com",
+  projectId: "blogproject-93123",
+  storageBucket: "blogproject-93123.appspot.com",
+  messagingSenderId: "982209235141",
+  appId: "1:982209235141:web:fa35b38063423fb3ef48ac"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
